fba870bd99acf7f9aafdd1681eecb87a0bd3df6b
Ben Biddington <ben.biddington@gmail.com>
2015-03-19 19:44:47 +1300

    Quantum Leap

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to solace
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/solace/core_test.clj
@@ -0,0 +1,7 @@
+(ns solace.core-test
+  (:require [clojure.test :refer :all]
+            [solace.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

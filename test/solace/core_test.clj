255477637a7771a89d9011e5a7eb43a43c0d82ae
Ben Biddington <ben.biddington@gmail.com>
2015-03-19 20:43:43 +1300

    it-does-not-save-invalid-values, for example -1

--- test/solace/core_test.clj
+++ test/solace/core_test.clj
@@ -2,23 +2,33 @@
   (:require [clojure.test :refer :all]
             [solace.core :refer :all]))
 
+(defn- valid?[n]
+  (> n 0))
+
 (defn add-solace [persistence n]
-  (persistence n))
+  (when (valid? n) 
+    (persistence n)))
 
 (def counter (atom 0))
 
 (def fake-persistence
   (fn[n] (swap! counter inc)))
 
-(defn- before-each[]
-  (reset! counter 0))
+(defn- before-each[f]
+  (reset! counter 0)
+  (f))
 
-(deftest adding-solace
-   (use-fixtures :each before-each)
+(use-fixtures :each before-each)
 
-  (testing "that it notifies persistence"
+(deftest adding-solace-notifies-persistence
+  (testing "that it calls it once exactly"
     (add-solace fake-persistence 1)
-    (is (= @counter 1), "Expected that the persistence function would have been called once"))
+    (is (= @counter 1), "Expected that the persistence function would have been called once")))
+
+(deftest it-does-not-save-invalid-values
+  (testing "for example -1"
+    (add-solace fake-persistence -1)
+    (is (= @counter 0), "Expected that the persistence function would NOT have been called at all because the value supplied is not valid"))
 
 ;; only supports 1-5
 ;; supports profanity: arse = 1, feck = 2, jubblies = 3, philbo = 4, ricky-bizzness = 5

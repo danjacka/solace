44693e27b791423b1f1f8a21914fc7f1dbac728b
Ben Biddington <ben.biddington@gmail.com>
2015-03-19 20:35:08 +1300

    adding-solace, testing "that it notifies persistence"

--- test/solace/core_test.clj
+++ test/solace/core_test.clj
@@ -2,6 +2,26 @@
   (:require [clojure.test :refer :all]
             [solace.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn add-solace [persistence n]
+  (persistence n))
+
+(def counter (atom 0))
+
+(def fake-persistence
+  (fn[n] (swap! counter inc)))
+
+(defn- before-each[]
+  (reset! counter 0))
+
+(deftest adding-solace
+   (use-fixtures :each before-each)
+
+  (testing "that it notifies persistence"
+    (add-solace fake-persistence 1)
+    (is (= @counter 1), "Expected that the persistence function would have been called once"))
+
+;; only supports 1-5
+;; supports profanity: arse = 1, feck = 2, jubblies = 3, philbo = 4, ricky-bizzness = 5
+;; any other tokens are rejected outright
+;; persistence not notified when invalid
+  )

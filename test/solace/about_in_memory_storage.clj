d4e09f774f283777b564d44237862a36cc573b3b
Ben Biddington <ben.biddington@gmail.com>
2015-03-20 14:50:31 +1300

    [GREEN] You can save and retrieve moods

--- test/solace/about_in_memory_storage.clj
+++ test/solace/about_in_memory_storage.clj
@@ -3,24 +3,40 @@
             [solace.core :refer :all]
             [solace.use-cases.add-solace :refer :all]))
 
-(defstruct quantum :mood)
-
 (def ^{:private true} store
   (atom {}))
 
-(defn save[n]
-  
-)
+(defn update [current what]
+  (let [n (keyword (str what))]
+    (let [current-n-value (get current n)]
+      (cond 
+        (nil? current-n-value) (merge current {n 1}) 
+        :else (update-in current [n] inc)))))
 
-(defn list-solace[]
-  (list)
-  )
+(defn save[what] (swap! store (fn [current] (update current what))))
 
-(deftest can-sav
-  (testing "that it calls it once exactly"
-    (save 1)
+(defn list-solace[] @store)
+
+(defn- before-each[f]
+  (reset! store {})
+  (f))
+
+(use-fixtures :each before-each)
+
+(deftest can-save
+  (testing "that you can add moods"
+    (save 1) (save 2) (save 3) (save 4) (save 5)
+        
     (let [result (list-solace)]
-      (is (= 1 (count result)))
-      )))
+      (is (= 1 (:1 result)))
+      (is (= 1 (:2 result)))
+      (is (= 1 (:3 result))))))
+
+(deftest can-save-with-either-a-number-or-a-string
+  (testing "that you can add moods"
+    (save 1) (save "1")
+        
+    (let [result (list-solace)]
+      (is (= 2 (:1 result)), "Expected the count to be <2> because we have added it twice"))))
 
 ;; it returns a struct containing the mood and a count

61300e46e88f376fe9f0f86ecd0d4fde438d58eb
Ben Biddington <ben.biddington@gmail.com>
2015-03-20 15:00:28 +1300

    Functions => file

--- test/solace/about_in_memory_storage.clj
+++ test/solace/about_in_memory_storage.clj
@@ -1,24 +1,10 @@
 (ns solace.about-in-memory-storage
   (:require [clojure.test :refer :all]
             [solace.core :refer :all]
-            [solace.use-cases.add-solace :refer :all]))
-
-(def ^{:private true} store
-  (atom {}))
-
-(defn update [current what]
-  (let [n (keyword (str what))]
-    (let [current-n-value (get current n)]
-      (cond 
-        (nil? current-n-value) (merge current {n 1}) 
-        :else (update-in current [n] inc)))))
-
-(defn save[what] (swap! store (fn [current] (update current what))))
-
-(defn list-solace[] @store)
+            [solace.adapters.in-memory-store :refer :all]))
 
 (defn- before-each[f]
-  (reset! store {})
+  (clear)
   (f))
 
 (use-fixtures :each before-each)
@@ -27,7 +13,7 @@
   (testing "that you can add moods"
     (save 1) (save 2) (save 3) (save 4) (save 5)
         
-    (let [result (list-solace)]
+    (let [result (list)]
       (is (= 1 (:1 result)))
       (is (= 1 (:2 result)))
       (is (= 1 (:3 result))))))
@@ -36,7 +22,7 @@
   (testing "that you can add moods"
     (save 1) (save "1")
         
-    (let [result (list-solace)]
+    (let [result (list)]
       (is (= 2 (:1 result)), "Expected the count to be <2> because we have added it twice"))))
 
 ;; it returns a struct containing the mood and a count
